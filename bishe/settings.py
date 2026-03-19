c45518fcaffd8ae4a2d0299ced1ce03a9a8bd417
xinhaiweiyi <41144111+xinhaiweiyi@users.noreply.github.com>
2026-03-19 15:19:05 +0800

    Change EMAIL_HOST_USER and EMAIL_HOST_PASSWORD defaults
    Updated email configuration to use empty defaults for user and password.

--- bishe/settings.py
+++ bishe/settings.py
@@ -134,8 +134,8 @@ EMAIL_BACKEND = os.getenv(
 )
 EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.qq.com")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", "587"))
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "2957524570@qq.com")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "yantmdaqqeftdcea")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "true").lower() == "true"
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
 
